03b9177494f4f2f9897448cbce4cd94af50bb3ff
LoaiEsam37 <loaiesam1234@gmail.com>
2023-03-23 14:46:22 +0200

    commit

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,77 +1,132 @@
 import "./styles/App.css";
+import { useState } from "react";
+import cardImage from "./assets/illustration-woman-online-mobile.svg";
 
 function App() {
+  const [collapse, setCollapse] = useState(0);
   return (
-    <div className="App">
-      <main>
-        <div class="card">
-          <div class="card__image">
-            <img src="" alt="card image" />
+    <main>
+      <div className="card">
+        <div className="card__image">
+          <img src={cardImage} alt="card image" />
+        </div>
+        <div className="card__content">
+          <div className="card__content--title">
+            <h1>FAQ</h1>
           </div>
-          <div class="card__content">
-            <div class="card__content--title">
-              <h1>FAQ</h1>
+          <div className="collapse">
+            <div className="collapse__button">
+              <button
+                onClick={() =>
+                  collapse === 1 ? setCollapse(0) : setCollapse(1)
+                }
+                className={collapse === 1 ? "active" : "disable"}
+              >
+                How many team members can I invite?
+              </button>
             </div>
-            <div class="collapse">
-              <div class="collapse__button">
-                <button>How many team members can I invite?</button>
-              </div>
-              <div class="collapse__body">
-                <p>
-                  You can invite up to 2 additional users on the Free plan.
-                  There is no limit on team members for the Premium plan.
-                </p>
-              </div>
+            <div
+              className={`collapse__body ${
+                collapse === 1 ? "visible" : "hidden"
+              }`}
+            >
+              <p>
+                You can invite up to 2 additional users on the Free plan. There
+                is no limit on team members for the Premium plan.
+              </p>
             </div>
-            <div class="collapse">
-              <div class="collapse__button">
-                <button>What is the maximum file upload size?</button>
-              </div>
-              <div class="collapse__body">
-                <p>
-                  No more than 2GB. All files in your account must fit your
-                  allotted storage space.
-                </p>
-              </div>
+          </div>
+          <div className="collapse">
+            <div className="collapse__button">
+              <button
+                onClick={() =>
+                  collapse === 2 ? setCollapse(0) : setCollapse(2)
+                }
+                className={collapse === 2 ? "active" : "disable"}
+              >
+                What is the maximum file upload size?
+              </button>
             </div>
-            <div class="collapse">
-              <div class="collapse__button">
-                <button>How do I reset my password?</button>
-              </div>
-              <div class="collapse__body">
-                <p>
-                  Click “Forgot password” from the login page or “Change
-                  password” from your profile page. A reset link will be emailed
-                  to you.
-                </p>
-              </div>
+            <div
+              className={`collapse__body ${
+                collapse === 2 ? "visible" : "hidden"
+              }`}
+            >
+              <p>
+                No more than 2GB. All files in your account must fit your
+                allotted storage space.
+              </p>
             </div>
-            <div class="collapse">
-              <div class="collapse__button">
-                <button>Can I cancel my subscription?</button>
-              </div>
-              <div class="collapse__body">
-                <p>
-                  Yes! Send us a message and we’ll process your request no
-                  questions asked.
-                </p>
-              </div>
+          </div>
+          <div className="collapse">
+            <div className="collapse__button">
+              <button
+                onClick={() =>
+                  collapse === 3 ? setCollapse(0) : setCollapse(3)
+                }
+                className={collapse === 3 ? "active" : "disable"}
+              >
+                How do I reset my password?
+              </button>
             </div>
-            <div class="collapse">
-              <div class="collapse__button">
-                <button>Do you provide additional support?</button>
-              </div>
-              <div class="collapse__body">
-                <p>
-                  Chat and email support is available 24/7. Phone lines are open
-                  during normal business hours.
-                </p>
-              </div>
+            <div
+              className={`collapse__body ${
+                collapse === 3 ? "visible" : "hidden"
+              }`}
+            >
+              <p>
+                Click “Forgot password” from the login page or “Change password”
+                from your profile page. A reset link will be emailed to you.
+              </p>
+            </div>
+          </div>
+          <div className="collapse">
+            <div className="collapse__button">
+              <button
+                onClick={() =>
+                  collapse === 4 ? setCollapse(0) : setCollapse(4)
+                }
+                className={collapse === 4 ? "active" : "disable"}
+              >
+                Can I cancel my subscription?
+              </button>
+            </div>
+            <div
+              className={`collapse__body ${
+                collapse === 4 ? "visible" : "hidden"
+              }`}
+            >
+              <p>
+                Yes! Send us a message and we’ll process your request no
+                questions asked.
+              </p>
+            </div>
+          </div>
+          <div className="collapse">
+            <div className="collapse__button">
+              <button
+                onClick={() =>
+                  collapse === 5 ? setCollapse(0) : setCollapse(5)
+                }
+                className={collapse === 5 ? "active" : "disable"}
+              >
+                Do you provide additional support?
+              </button>
+            </div>
+            <div
+              className={`collapse__body ${
+                collapse === 5 ? "visible" : "hidden"
+              }`}
+            >
+              <p>
+                Chat and email support is available 24/7. Phone lines are open
+                during normal business hours.
+              </p>
             </div>
           </div>
         </div>
-      </main>
-    </div>
+      </div>
+    </main>
   );
 }
 
